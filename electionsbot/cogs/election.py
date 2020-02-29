132b86772a98057fd199cda66b8542bc4395115d
Daniel Milnes <thebeanogamer@gmail.com>
2020-02-29 22:44:01 +0000

    Better variable names

--- electionsbot/cogs/election.py
+++ electionsbot/cogs/election.py
@@ -5,7 +5,7 @@ from json import load
 import random
 import urllib.request
 import re
-from ..constants import EMOJI_SERVER_ID
+from electionsbot.constants import EMOJI_SERVER_ID
 
 
 class ElectionCog(commands.Cog):
@@ -61,7 +61,7 @@ class ElectionCog(commands.Cog):
 
     @commands.command()
     async def candidateList(self, ctx):
-        names = [x.username for x in self.getAllCandidates()]
+        names = [candidate.username for candidate in self.getAllCandidates()]
         print(names)
         await ctx.send("In a random order, the candidates currently standing are: \n" + "\n".join(names))
 
@@ -73,13 +73,13 @@ class ElectionCog(commands.Cog):
 
     @commands.command()
     async def voteDM(self, ctx):
-        m = await ctx.send("Click on the reactions representing the users you wish to vote for. Once you're done, react"
+        message = await ctx.send("Click on the reactions representing the users you wish to vote for. Once you're done, react"
                            " with a ✅ to confirm. **Remember, you can only vote for two candidates, and you can't change"
                            " your mind once you confirm!**"
                            "\n*This prompt will expire in 5 minutes.*")
-        for c in [x.emoji for x in self.getAllCandidates()]:
-            await m.add_reaction(c)
-        await m.add_reaction("✅")
+        for emoji in [candidate.emoji for candidate in self.getAllCandidates()]:
+            await message.add_reaction(emoji)
+        await message.add_reaction("✅")
 
 
 class Candidate:
