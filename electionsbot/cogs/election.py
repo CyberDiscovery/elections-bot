e0d92221829b3c065bcce43f1812b1234f5d2d4b
picapi <picapi123@gmail.com>
2020-03-01 19:50:45 +0000

    Changed to sample data and updated time for final testing

--- electionsbot/cogs/election.py
+++ electionsbot/cogs/election.py
@@ -30,7 +30,7 @@ async def connectPostgres():
 class ElectionCog(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
-        data = load(open("applications.json", "r", encoding="UTF-8"))
+        data = load(open("testdata.json", "r", encoding="UTF-8"))
         self.candidateData = data["candidates"]
         self.candidates = {}
         self.voteSessions = {}
